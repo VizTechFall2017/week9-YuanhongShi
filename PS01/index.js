06fc60c22a31ab1f1ecb37680b90df207df8c9e4
YuanhongShi <31866504+YuanhongShi@users.noreply.github.com>
2017-11-13 17:09:23 -0500

    built up the select form coding structure

--- PS01/index.js
+++ PS01/index.js
@@ -22,9 +22,181 @@ var albersProjection = d3.geoAlbers()
 var path = d3.geoPath()
 .projection(albersProjection);
 
-var selectedCity = '';
+var arrayList = [
+    {long:-71.056612, lat:42.354175}
+];
+
+var cityArrary = ['BOSTON IN MA',
+                    'NEW YORK IN NY',
+                    'CHICAGO IN CA',
+                    'LOS ANGELES IN IL',
+                    'PHOENIX IN AZ',
+                    'SEATTLE IN WA'];
+
+
+arrayList = [
+    {long:-71.056612, lat:42.354175}
+];
+
+
+
+
+d3.select('select')
+    .on('change', function(d){
+        var selectCity = d3.select('select').property('value');
+        //console.log(selectCity);
+
+        if (selectCity == cityArrary[0]) {
+            console.log(selectCity);
+            d3.json('./neighborhood_boston.json', function(dataIn){
+                //console.log(dataIn);
+                svg1.selectAll('path')
+                    .data(dataIn.features)
+                    .enter()
+                    .append('path')
+                    .attr('d', path)
+                    .attr('fill', 'gainsboro')
+                    .attr('stroke', 'white')
+                    .attr('stroke-width', 1)
+                    .attr('data-toggle',"tooltip")
+                    .attr('title', function(d){
+                        if (d.properties.name == 'Downtown'){
+                            return 'CLICK ON THE DOT!'
+                        }
+                        else
+                            return d.properties.name;
+                    })
+                    .on('mouseover', function(d){
+                        if (d.properties.name == 'Downtown'){
+                            d3.select(this)
+                                .attr('fill', 'red');
+                        }
+                        else{
+                            d3.select(this)
+                                .attr('fill', 'yellow');
+                        }
+
+                    })
+                    .on('mouseout', function(d){
+                        d3.select(this)
+                            .attr('fill', 'gainsboro');
+                    })
+                    .on('click', function(d){
+
+                    });
+
+                //   $('[data-toggle="tooltip"]').tooltip();
+
+                svg1.selectAll('circle')
+                    .data(arrayList)
+                    .enter()
+                    .append('circle')
+                    .attr('cx', function (d){
+                        return albersProjection([d.long, d.lat])[0];
+                    })
+                    .attr('cy', function (d){
+                        return albersProjection([d.long, d.lat])[1];
+                    })
+                    .attr('r', 5)
+                    .attr('fill', 'steelblue')
+                    .on('mouseover', function(d){
+                        console.log('show some thing');
+                        d3.select(this)
+                        //why this tooltip not work!
+                        //.attr('data-toggle',"tooltip")
+                            .attr('data-content', 'CLICK ON ME!')
+                            .transition()
+                            .duration(3000)
+                            .ease(d3.easeBounce)
+                            .attr('fill', 'red')
+                            .attr('r', 20)
+                            .attr('opacity', .8)
+                            .attr('stroke', 'white')
+                            .attr('stroke-width', 0.5);
+
+                    })
+
+                    .on('mouseout', function(d){
+                        d3.select(this)
+                            .transition()
+                            .duration(3000)
+                            .ease(d3.easeBounce)
+                            .attr('fill', 'steelblue')
+                            .attr('stroke', 'none')
+                            .attr('r', 5);
+                    })
+
+                    .on('click', function(d){
+                        d3.select(this)
+                            .attr('fill', 'red')
+                            .attr('r', 10);
+                        d3.select('.background')
+                            .transition()
+                            .duration(3000)
+                            .ease(d3.easeBounce)
+                            .attr('opacity', 1);
+
+                    });
+
+                $('[data-toggle="tooltip"]').tooltip();
+
+
+            });
+
+            //var svg2Background = 'Boston';
+
+
+        }
+
+        else if (selectCity == cityArrary[1]){
+            return;
+        }
+        else if (selectCity == cityArrary[2]){
+            return;
+        }
+        else if (selectCity == cityArrary[3]){
+            return;
+        }
+        else if (selectCity == cityArrary[4]){
+            return;
+        }
+        else if (selectCity == cityArrary[5]){
+            return;
+        }
+
+
+        var defs = svg2.append('defs');
+        defs.append('pattern')
+            .attr('id','bg')
+            .attr('patternUnits', 'userSpaceOnUse')
+            .attr('width', width)
+            .attr('height', height)
+            .append('image')
+            .attr('xlink:href', function(d){
+                return selectCity + '.png';
+
+            })
+            .attr('width', width)
+            .attr('height', height)
+            .attr('x', 0)
+            .attr('y', 0);
+
+        svg2.append('rect')
+            .attr('class', 'background')
+            .attr('width', width)
+            .attr('height', height)
+            .attr('fill', 'url(#bg)')
+            .attr('opacity', 0);
+
+
+    });
+
+
+
+
 
 //import the data from the .csv file
+/*
 d3.json('./neighborhood_boston.json', function(dataIn){
     //console.log(dataIn);
     svg1.selectAll('path')
@@ -120,12 +292,7 @@ d3.json('./neighborhood_boston.json', function(dataIn){
 
 });
 
-arrayListtest= [
-    {long:-71.090508, lat:42.340052},
-    {long:-71.086145, lat:42.338542},
-    {long:-71.081518, lat:42.341663},
-    {long:-71.076440, lat:42.348695}
-];
+
 
 
 
@@ -178,6 +345,7 @@ d3.select('#show_point')
 
     });
     });
+*/
 
 /*d3.select('#show_path')
     .on('click', function() {
